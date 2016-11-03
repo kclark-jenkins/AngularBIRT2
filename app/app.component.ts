b909d93a9c80be7fc3e3ac483134a2da06610d86
Kristopher Clark <krisclarkdev@krisbox.org>
2016-11-03 15:21:15 -0500

    Added templates folder

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    templateUrl: 'templates/index.tmpl.html'
 })
 export class AppComponent { }
